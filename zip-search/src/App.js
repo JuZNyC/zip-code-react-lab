9e33e96eb6b3c9971293a05b13d5ac23ff720506
JuZNyC <junz.nyc@gmail.com>
2021-10-05 06:31:51 -0400

    push part 1

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -7,25 +7,45 @@ function City(props) {
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+      <div>
+        Zip Code:
+        <input type="text" onChange={props.changeHandler}/>
+      </div>
+  );
 }
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Zip Code Search</h2>
-        </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
-        </div>
-      </div>
-    );
-  }
+
+    state = {
+        zipCode: ''
+    }
+
+    zipChanged = (event) => {
+        console.log(event.target.value)
+        this.setState({ zipCode: event.target.value })
+
+        fetch(`http://ctp-zip-api.herokuapp.com/zip/${event.target.value}`)
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+    }
+
+    render() {
+        return (
+          <div className="App">
+            <div className="App-header">
+              <h2>Zip Code Search</h2>
+            </div>
+            <ZipSearchField zipCode={this.state.zipCode} changeHandler={this.zipChanged} />
+            <div>Current Zip is { this.state.zipCode }</div>
+            <div>
+              <City />
+              <City />
+            </div>
+          </div>
+        );
+    }
 }
 
 export default App;
